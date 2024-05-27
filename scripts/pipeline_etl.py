4b97121f609a25ae89a66e873581db7b40556860
louis93160 <lduvieux@laposte.net>
2024-05-28 01:44:22 +0200

    Deuxième commit

--- scripts/pipeline_etl.py
+++ scripts/pipeline_etl.py
@@ -1,22 +1,60 @@
 from pymongo import MongoClient
-from pymongo.errors import ConnectionFailure
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+# Connexion à MongoDB
+client = MongoClient("mongodb://localhost:27017/")
+db = client["Blent"]
+collection = db["VideoGame"]
 
 
+# Définir la date de fin et calculer la date de début 6 mois avant
+fin = datetime(2018, 10, 2)
+debut = fin - relativedelta(months=6)
 
-def connexion_mongo(uri, db_name):
+pipeline = [
+    {
+        '$match': {
+            'unixReviewTime': {
+                '$gt': int(debut.timestamp()), 
+                '$lt': int(fin.timestamp())
+            }
+        }
+    },
+    {
+        '$group': {
+            '_id': '$asin',
+            'averageRating': {'$avg': '$overall'},
+            'numUsersRated': {'$sum': 1},
+            'oldestRating': {'$min': '$unixReviewTime'},
+            'newestRating': {'$max': '$unixReviewTime'}
+        }
+    },
+    {
+        '$addFields': {
+            'averageRating': {'$round': ['$averageRating', 1]},
+            'oldestRating': {
+                '$dateToString': {
+                    'format': '%Y-%m-%d', 
+                    'date': {'$toDate': {'$multiply': ['$oldestRating', 1000]}}
+                }
+            },
+            'newestRating': {
+                '$dateToString': {
+                    'format': '%Y-%m-%d', 
+                    'date': {'$toDate': {'$multiply': ['$newestRating', 1000]}}
+                }
+            }
+        }
+    },
+    {
+        '$sort': {'_id': 1}
+    }
+]
 
+# Exécuter le pipeline d'agrégation
+result = list(collection.aggregate(pipeline))
 
-    try:
-        client = MongoClient(uri)
-        db = client[db_name]
-        db.command("ping")
-        print("Connexion à MongoDB réussie")
-        return db
-    except ConnectionFailure as e:
-        print(f"Connexion à MongoDB échoué: {e}")
-        return None
-
-uri = "mongodb://127.0.0.1:27017"
-db_name = "Blent"
-
-db = connexion_mongo(uri, db_name)
+# Afficher les résultats
+for doc in result:
+    print(doc)
